22773f9d2c447d0dffa5fe30fbba44eb19920a7e
Jeffrey <jeffreycohenit@gmail.com>
2023-01-09 23:46:12 +0000

    2nd code push

--- networking/variables.tf
+++ networking/variables.tf
@@ -1,3 +1,13 @@
 # --- networking/variables.tf ---
 
-variable "vpc_cidr" {}
+variable "vpc_cidr" {
+    type = string
+}
+
+variable "public_cidrs" {
+    type = list
+}
+
+variable "private_cidrs" {
+    type = list
+}
