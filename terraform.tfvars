4a7a6a0fecb1867206a2056232f3d138367aed0e
Jeffrey <jeffreycohenit@gmail.com>
2023-01-10 14:06:47 +0000

    Uploading changes to my terraform deployment

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,3 +1,9 @@
 # --- root/terraform.tfvars ---
 
 access_ip = "0.0.0.0/0"
+
+#--db vars --
+
+db_name    = "rancher"
+dbuser     = "bobby"
+dbpassword = "suP3rte33b00tz88!"
--- variables.tf
+++ variables.tf
@@ -7,3 +7,17 @@ variable "aws_region" {
 variable "access_ip" {
   type = string
 }
+
+#-------database variables
+
+variable "db_name" {
+  type = string
+}
+variable "dbuser" {
+  type      = string
+  sensitive = true
+}
+variable "dbpassword" {
+  type      = string
+  sensitive = true
+}
